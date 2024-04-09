796243c891ef8fe2e9258c274065b5c55cead477
Henrique Mendes <henriquemendes13@gmail.com>
2024-04-09 06:50:14 -0300

    Add project files.

---
+++ Controllers/ProdutosController.cs
@@ -0,0 +1,103 @@
+﻿using ApiFuncional.Data;
+using ApiFuncional.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiFuncional.Controllers
+{
+    [Route("api/produtos")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly ApiDbContext _context;
+
+        public ProdutosController(ApiDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult<IEnumerable<Produto>>> GetProdutos()
+        {
+            if (_context.Produtos == null) return NotFound();
+
+            return await _context.Produtos.ToListAsync();
+        }
+
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult<Produto>> GetProduto(int id)
+        {
+            if (_context.Produtos == null) return NotFound();
+
+            var produto = await _context.Produtos.FindAsync(id);
+
+            if (produto == null) return NotFound();
+
+            return produto;
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult<Produto>> PostProduto(Produto produto)
+        {
+            if (_context.Produtos == null) return Problem("Erro ao criar um produto");
+
+            if (!ModelState.IsValid)
+            {
+                return ValidationProblem(new ValidationProblemDetails(ModelState)
+                {
+                    Title = "Um ou mais erros de validação ocorreram."
+                });
+            }
+
+            _context.Produtos.Add(produto);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetProduto), new { id = produto.Id }, produto);
+        }
+
+        [HttpPut("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult<Produto>> PutProduto(int id, Produto produto)
+        {
+            if (id != produto.Id) return BadRequest();
+
+            if (!ModelState.IsValid) return ValidationProblem(ModelState);
+
+            _context.Produtos.Update(produto);
+            
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult<Produto>> DeleteProduto(int id)
+        {
+            if (_context.Produtos == null) return NotFound();
+
+            var produto = await _context.Produtos.FindAsync(id);
+
+            if (produto == null) return NotFound();
+
+            _context.Produtos.Remove(produto);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+using ApiFuncional.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApiDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<ApiDbContext>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
